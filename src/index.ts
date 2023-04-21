8940627ccf4ed01a8582a48b721c6acddeaefdec
bwillem <bguenther3@gmail.com>
2023-04-21 09:38:04 -0700

    get pdf upload working

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,19 @@ const generatePdf: HttpFunction = async (req, res) => {
   const { survey_id } = req.query
   if (!survey_id) throw new Error('Missing survey_id')
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, args: ['--user-agent=LST'] });
   const page = await browser.newPage()
   const url = `https://lifestresstest.com/stress-test-report?dd=${survey_id}`
 
   console.log('navigating to ', url)
 
-  await page.goto(url, { waitUntil: 'networkidle0' })
-  const file = await page.pdf()
+  await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 })
+  const buffer = await page.pdf()
 
   console.debug('file created')
 
   await browser.close();
-  await storageService.upload({ filename: `${survey_id}-report.pdf`, file })
+  await storageService.upload({ filename: `${survey_id}-report.pdf`, buffer })
 
   res.sendStatus(200)
 }
--- src/storage.ts
+++ src/storage.ts
@@ -1,35 +1,38 @@
 import * as fetch from 'isomorphic-fetch'
 import * as FormData from 'form-data'
+import { Blob } from 'buffer'
 
 interface UploadOpts {
     filename: string
-    file: Buffer
+    buffer: Buffer
 }
 
-const upload = async ({ filename, file }: UploadOpts) => {
+const upload = async ({ filename, buffer }: UploadOpts) => {
     const formData = new FormData()
-
-    formData.append('file', file)
-    formData.append('title', filename)
-    formData.append('caption', 'Report file')
 
     console.debug('uploading file: ', filename)
 
+    formData.append('file', buffer, filename)
+    formData.append('title', filename)
+    formData.append('caption', 'Report file')
+
     try {
         if (!process.env.password) throw new Error('Missing password')
-        await fetch('https://lifestresstest.com/wp-json/wp/v2/media', {
+        const r = await fetch('https://lifestresstest.com/wp-json/wp/v2/media', {
             method: 'post',
             headers: {
-                Authorization: `Basic admin:${Buffer.from(process.env.password).toString('base64')}`
+                Authorization: `Basic ${Buffer.from(`admin:${process.env.password}`).toString('base64')}`
+                // Authorization: `Basic admin:${process.env.password}`
             },
             body: formData,
         })
+        if (!r.ok) throw new Error(JSON.stringify(r.body))
+        console.debug(JSON.stringify(r.body))
         console.debug('file uploaded successfully')
     } catch (e) {
         console.error(e)
-        throw new Error('Saving file to GCS failed')
+        throw new Error('File upload failed')
     }
-
 }
 
 const storageService = {
